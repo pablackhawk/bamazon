bde877e4555d6ff7fd5a13bcd83018e966f50e12
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-06 23:12:37 -0800

    Modified schema to be in line with MySQL conventions

--- schema.sql
+++ schema.sql
@@ -3,15 +3,10 @@ CREATE DATABASE bamazon;
 USE bamazon;
 CREATE TABLE products
 (
-  item_id INT NOT NULL
-  AUTO_INCREMENT,
-  product_name VARCHAR
-  (45) NOT NULL,
-  department VARCHAR
-  (45),
-  price DECIMAL
-  (13,2) NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR (45) NOT NULL,
+  department VARCHAR (45),
+  price DECIMAL (13,2) NOT NULL,
   stock_quantity DECIMAL,
-  PRIMARY KEY
-  (item_id)
+  PRIMARY KEY (item_id)
 );
