4892c3f2efb573c94f0a59b25d0426b305128fa9
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-07 12:57:58 -0800

    Added manager functionality and updated README

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ A command-line interface mirroring the capabilities of Amazon utilizing the npm 
 The customer interface allows the user to view available items for sale. The user can then select an item available for sale. If the item is currently in stock then a verification message will appear and the application exits. If the desired item or the quantity desired is not available, an alert will tell the customer and prompt for another item.
 
 To see a demonstration of the customer application <a href="https://youtu.be/Y-jJxi3aPkA"> Click Here!</a>
+
+### Manager Interface
+
+The manager interface allows the user to:
+
+* View available items for sale
+* View low inventory (less than 10 items in stock)
+* Add inventory
+* Add new products
+
+### Author
+
+Laurentius Tirtarahardja
+_Copyright &copy; 2018 Laurentius Tirtarahardja_
--- managerView.js
+++ managerView.js
@@ -88,7 +88,180 @@ function displayInventory() {
       res += 'Department: ' + data[i].department + '\n'
       res += 'Price: $' + parseFloat(data[i].price).toFixed(2) + '\n'
       console.log(res + '\n----------------------------------------\n')
-      connection.end()
     }
+    connection.end()
   })
 }
+
+// displayLow Inventory dislays a list of products where inventory is below 10
+
+function displayLowInventory() {
+  queryStr = 'SELECT * FROM products WHERE stock_quantity < 10'
+
+  connection.query(queryStr, function(err, data) {
+    if (err) throw err
+
+    console.log(
+      'Low Inventory (quantity below 10) \n----------------------------------------\n'
+    )
+
+    let res = ''
+    for (let i = 0; i < data.length; i++) {
+      res = ''
+      res += 'Item ID: ' + data[i].item_id + '\n'
+      res += 'Product Name: ' + data[i].product_name + '\n'
+      res += 'Department: ' + data[i].department + '\n'
+      res += 'Price: $' + parseFloat(data[i].price).toFixed(2) + '\n'
+      console.log(res + '\n----------------------------------------\n')
+    }
+    connection.end()
+  })
+}
+
+// Ensures user is supplying only positive integers
+function validateInteger(value) {
+  let integer = Number.isInteger(parseFloat(value))
+  let sign = Math.sign(value)
+
+  if (integer && sign === 1) {
+    return true
+  } else {
+    return 'Please enter a whole non-zero number'
+  }
+}
+
+function validateNumeric(value) {
+  let number = typeof parseFloat(value) === 'number'
+  let positive = parseFloat(value) > 0
+
+  if (number && positive) {
+    return true
+  } else {
+    return 'Please enter a positive number for unit price'
+  }
+}
+
+//This will add inventory to existing items
+function addInventory() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'item_id',
+        message: 'Please enter the item Id for stock quantity update',
+        validate: validateInteger,
+        filter: Number,
+      },
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many would you like to add?',
+        validate: validateInteger,
+        filter: Number,
+      },
+    ])
+    .then(function(input) {
+      let item = input.item_id
+      let addQuantity = input.quantity
+      let queryStr = 'SELECT * FROM products WHERE ?'
+
+      connection.query(queryStr, { item_id: item }, function(err, data) {
+        if (err) throw err
+        if (data.length === 0) {
+          console.log('ERROR: Invalid Item ID. Please enter a valid Item ID.')
+          addInventory()
+        } else {
+          let productData = data[0]
+          console.log('Updating Inventory...')
+          let updateQueryStr =
+            'UPDATE products SET stock_quantity = ' +
+            (productData.stock_quantity + addQuantity) +
+            ' WHERE item_id = ' +
+            item
+
+          connection.query(updateQueryStr, function(err, data) {
+            if (err) throw err
+            console.log(
+              'Stock count for Item ID ' +
+                item +
+                ' has been updated to ' +
+                (productData.stock_quantity + addQuantity) +
+                '.\n----------------------------------------\n'
+            )
+            connection.end()
+          })
+        }
+      })
+    })
+}
+
+// Guides user to adding a new product to the database
+function createNewProduct() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'product_name',
+        message: 'Please enter the new product name:',
+      },
+      {
+        type: 'input',
+        name: 'department_name',
+        message: 'Please enter the department this product belongs to',
+      },
+      {
+        type: 'input',
+        name: 'price',
+        message: 'What is the price per unit?',
+        validate: validateNumeric,
+      },
+      {
+        type: 'input',
+        name: 'stock_quantity',
+        message: 'How many items are being stocked?',
+        valifate: validateInteger,
+      },
+    ])
+    .then(function(input) {
+      console.log(
+        'Adding New Item: \n    product_name = ' +
+          input.product_name +
+          '\n' +
+          '    department_name = ' +
+          input.department_name +
+          '\n' +
+          '    price = ' +
+          input.price +
+          '\n' +
+          '    stock_quantity = ' +
+          input.stock_quantity
+      )
+      let queryStr = 'INSERT into products SET ?'
+
+      connection.query(
+        queryStr,
+        {
+          product_name: input.product_name,
+          department: input.department_name,
+          price: input.price,
+          stock_quantity: input.stock_quantity,
+        },
+        function(err, results, fields) {
+          if (err) throw err
+          console.log(
+            'New product has been added to the inventory under Item ID ' +
+              results.insertId +
+              '.\n----------------------------------------\n'
+          )
+          connection.end()
+        }
+      )
+    })
+}
+
+// Runs main app logic
+function runBamazon() {
+  managerAction()
+}
+
+runBamazon()
